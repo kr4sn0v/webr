b2ce03d872a70c10868f64fd4e22e24215e52828
Igor Krasnov <qiuscou@icloud.com>
2026-03-03 13:54:16 +0500

    Исправлена ошибка с отображением полей ввода

--- frontend/src/components/base/InputBase.vue
+++ frontend/src/components/base/InputBase.vue
@@ -1,7 +1,10 @@
 <template>
-  <Field class="w-full rounded-md border border-gray-300 bg-white p-2">
-    <slot></slot>
-  </Field>
+  <Field
+    v-if="$route.path === '/users'"
+    class="w-full rounded-md border border-gray-300 bg-white p-2"
+    ><slot></slot
+  ></Field>
+  <Field v-else class="w-full rounded-md border border-gray-300 bg-white p-2"></Field>
 </template>
 
 <script setup>
